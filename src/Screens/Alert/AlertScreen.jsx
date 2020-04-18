a4d1fb4d6515bd3d8c7e1a73006f2f1713d3bd50
tjjone98 <tzero1898@gmail.com>
2020-04-18 17:05:44 +0700

    fix[alert]: add alert screen

--- src/Screens/Alert/AlertScreen.jsx
+++ src/Screens/Alert/AlertScreen.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, Dimensions} from 'react-native';
 import {observer} from 'mobx-react';
 import colors from '../../Themes/colors';
 import statsStore from '../../Stores/Stats/Stats';
 import Icon from 'react-native-vector-icons/Ionicons';
 import fonts from '../../Themes/fonts';
+import Symptom from '../../Assets/Svgs/symptom.svg';
+import Covid19 from '../../Assets/Svgs/Covid19.svg';
+const screenWidth = Dimensions.get('window').width;
 
 const AlerScreen = observer(() => {
   return (
@@ -20,10 +23,95 @@ const AlerScreen = observer(() => {
             />
           </View>
           <View style={styles.title}>
-            <Text style={styles.textTitle}>Alert</Text>
+            <Text style={[styles.textTitle, {fontWeight: 'bold'}]}>
+              What is COVID-19?
+            </Text>
           </View>
         </View>
-        <View style={styles.bottom}></View>
+        <View style={styles.bottom}>
+          <View style={styles.content}>
+            <Text
+              style={[
+                styles.textTitle,
+                {
+                  fontWeight: 'bold',
+                  color: colors.redCallNow,
+                  marginBottom: 5,
+                },
+              ]}>
+              COVID-19
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                paddingTop: 20,
+                paddingBottom: 20,
+                paddingLeft: 5,
+                paddingRight: 5,
+                borderWidth: 0.4,
+                borderRadius: 10,
+                marginBottom: 20,
+                borderColor: 'transparent',
+                backgroundColor: '#F9FAFC',
+              }}>
+              <Covid19 width={90} height={90} />
+              <Text
+                style={[
+                  styles.textTitle,
+                  {
+                    fontSize: fonts.sm,
+                    color: 'black',
+                    marginBottom: 5,
+                    width: '60%',
+                  },
+                ]}>
+                Coronavirus disease 2019 (COVID-19) is an infectious disease
+                caused by severe acute respiratory syndrome coronavirus 2
+                (SARS-CoV-2).
+              </Text>
+            </View>
+
+            <Text
+              style={[
+                styles.textTitle,
+                {fontWeight: 'bold', color: colors.redCallNow, marginBottom: 5},
+              ]}>
+              COMMON
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row-reverse',
+                alignItems: 'center',
+                justifyContent: 'space-around',
+                paddingTop: 20,
+                paddingBottom: 20,
+                paddingLeft: 5,
+                paddingRight: 5,
+                borderWidth: 0.4,
+                borderRadius: 10,
+                borderColor: 'transparent',
+                backgroundColor: '#F9FAFC',
+              }}>
+              <Symptom width={90} height={90} />
+              <Text
+                style={[
+                  styles.textTitle,
+                  {
+                    fontSize: fonts.sm,
+                    color: 'black',
+                    marginBottom: 5,
+                    width: '60%',
+                  },
+                ]}>
+                Common symptoms include fever, cough, and shortness of breath.
+                Other symptoms may include fatigue, muscle pain, diarrhea, sore
+                throat, loss of smell, and abdominal pain.
+              </Text>
+            </View>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -33,7 +121,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   top: {
-    flex: 1,
+    flex: 2,
     paddingLeft: 24,
     paddingRight: 24,
     paddingTop: 20,
@@ -49,13 +137,18 @@ const styles = StyleSheet.create({
   textTitle: {
     fontSize: fonts.xl,
     color: colors.white,
-    fontWeight: 'bold',
   },
   bottom: {
-    flex: 3,
+    flex: 4,
     backgroundColor: colors.white,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+  content: {
+    marginTop: 20,
+    justifyContent: 'center',
   },
 });
 
